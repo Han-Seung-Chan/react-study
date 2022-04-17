c27d82df1c9403a1ebfc3140876f139625feef42
seungchanHan <seungchanhan00@gmail.com>
2022-04-17 23:58:05 +0900

    add : 각 컴포넌트 연결 및 css 작업

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -41,8 +41,16 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  { id: 1, emotion: 1, content: '오늘 일기 1번', date: 1650125930758 },
+  { id: 2, emotion: 2, content: '오늘 일기 2번', date: 1650125930759 },
+  { id: 3, emotion: 3, content: '오늘 일기 3번', date: 1650125930760 },
+  { id: 4, emotion: 4, content: '오늘 일기 4번', date: 1650125930761 },
+  { id: 5, emotion: 5, content: '오늘 일기 5번', date: 1650125930762 },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
 
   const dataId = useRef(0);
   //CREATE
